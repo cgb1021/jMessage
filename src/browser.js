c1616410a40578f12d25ef20f32fd604ea911ddd
chenguibiao <chenguibiao@yy.com>
2019-05-15 17:57:56 +0800

    todo: create,dragEvent

---
+++ src/browser.js
@@ -0,0 +1,43 @@
+const documentElement = document.documentElement;
+let transform= false, //transform
+  transform3d= false, //transform translate3d
+  hasTransformPrefix = false,
+  prefix = '';
+
+if ('addEventListener' in window) {
+  //标准浏览器或者ie9以上
+  let style = global.getComputedStyle(documentElement);
+  try {
+    //css前缀(浏览器前缀)
+    prefix = [].slice.call(style).join('').match(/-(webkit|ms|moz|o)-/i)[1];
+  } catch (e) {
+    console.log('no css prefix info', e)
+  }
+  
+  transform = typeof style.transform === 'string' || typeof style[prefix + 'Transform'] === 'string';
+  if (transform) {
+    hasTransformPrefix = !(typeof style.transform === 'string');
+    let str = '';
+    if (!hasTransformPrefix) {
+      documentElement.style.transform = 'translate3d(0,0,0)';
+      str = 'transform';
+    } else {
+      str = `-${prefix}-transform`;
+      documentElement.style[str] = 'translate3d(0,0,0)';
+    }
+    style = global.getComputedStyle(documentElement);
+    transform3d = /^\w{5}/.test(style[str]); // 不为空，非none
+    documentElement.style.transform = documentElement.style[`-${prefix}-transform`] = '';
+  }
+  //清理
+  style = null;
+} else {
+  prefix = 'ms';
+}
+
+export {
+  prefix,
+  transform,
+  transform3d,
+  hasTransformPrefix
+}
--- src/message.js
+++ src/message.js
@@ -1,8 +1,10 @@
+import { prefix as cssPrefix, transform, transform3d, hasTransformPrefix } from './browser';
 const message = {
   option: {
     zIndex: 999,
     activeClassName: '',
-    maskClassName: ''
+    maskClassName: '',
+    transform: true, // 是否使用transform属性拖动
   }, // 全局自定义配置
   box: null,
   length: 0
@@ -19,7 +21,6 @@ defaultOption = {
   className: '', // 附加css类名
   showMask: true, // 是否显示遮罩层
   timeout: 0, // 多少秒后自动关闭
-  transform: true, // 是否使用transform属性拖动
   noClose: false, // 不允许关闭
   dragMode: 2 // 0不允许移动,1整个移动,2允许head移动
 }, //弹窗默认配置
@@ -29,18 +30,24 @@ mask = {
     this.node && this.node.classList.add('hide');
   },
   show() {
-    if (this.node || !appendMask()) {
-      this.node.className = `${maskClassName} ${message.option.maskClassName}`;
+    if (!this.node) {
+      // 生成mask
+      const node = this.node = document.createElement('div');
+      node.style.zIndex = message.option.zIndex;
+      rootNode.appendChild(node);
+
+      node.addEventListener('click', function () {
+        !currentBox.option.noClose && counter <= 1 && exit(-1);
+      });
     }
+    this.node.className = `${maskClassName} ${message.option.maskClassName}`;
   }
-} //蒙层对象
+}, //蒙层对象
+transformStyle = transform3d ? 'translate3d({$position},0)' : 'translate({$position})',
+transformStr = !hasTransformPrefix ? 'transform' : `${cssPrefix}Transform`
 ;
 let currentBox = null, //当前弹窗对象
-counter = 0, //当前弹窗个数计数器
-transform = true,
-transform3d = true,
-hasTransformPrefix = false,
-cssPrefix = ''
+counter = 0 //当前弹窗个数计数器
 ;
 
 rootNode.id = 'jmessage';
@@ -54,7 +61,7 @@ document.body.addEventListener('keydown', function (e) {
     e.stopPropagation();
     let index = -2;
 
-    if (key == 13) {
+    if (key === 13) {
       // enter键
       let foot = currentBox.node.lastElementChild,
         btn = foot && foot.classList.contains(`${className}__foot`) ? foot.querySelector('button:focus') : null;
@@ -69,16 +76,12 @@ document.body.addEventListener('keydown', function (e) {
     exit(index);
   }
 });
-
-function appendMask () {
-  const node = mask.node = document.createElement('div');
-  node.className = maskClassName;
-  node.style.zIndex = message.option.zIndex;
-  rootNode.appendChild(node);
-
-  node.addEventListener('click', function () {
-    !currentBox.option.noClose && counter <= 1 && exit(-1);
-  });
+/*
+ * 退出
+ */
+function exit(index = -1) {
+  counter && currentBox.remove(index);
+  !counter && mask.hide();
 }
 function clickEvent(self, index) {
   return function (e) {
@@ -94,40 +97,34 @@ function clickEvent(self, index) {
  * @param object option 配置对象
  */
 function dragEvent (box) {
-  var moveEnable = false, //是否允许移动
-    node = box.node,
-    dragMode = box.option.dragMode,
+  const dragMode = box.option.dragMode,
     data = {
-      width: node.offsetWidth, //弹窗宽度
-      height: node.offsetHeight, //弹窗高度
+      width: box.node.offsetWidth, //弹窗宽度
+      height: box.node.offsetHeight, //弹窗高度
       startX: 0, //鼠标/触摸初始x轴坐标
       startY: 0, //鼠标/触摸初始y轴坐标
       x: 0, //弹窗初始x轴坐标
       y: 0, //弹窗初始y轴坐标
       offsetLeft: 0, //transform模式下，弹窗的offsetLeft
       offsetTop: 0 //transform模式下，弹窗的offsetTop
-    },
-    transformStyle = '', //style.transform的值 分2d和3d
-    transformStr = '' //style.transform
+    };
+  let enable = false //是否允许移动
   ;
 
   //确定拖动模式
-  if (box.option.transform && transform) {
+  if (message.option.transform && transform) {
     //transform模式
-    transformStyle = transform3d ? 'translate3d({$position},0)' : 'translate({$position})';
-    transformStr = !hasTransformPrefix ? 'transform' : cssPrefix + 'Transform';
-
     return function (e) {
-      var type = e.type;
+      const type = e.type;
       switch (type) {
         //移动中
         case 'touchmove':
         case 'mousemove': {
-          if (moveEnable) {
+          if (enable) {
             e.preventDefault();
             e.stopPropagation();
             //var replaceStr = type == 'touchmove' ? Math.floor(data.x + (e.targetTouches[0].pageX - data.startX)) + 'px,' + Math.floor(data.y + (e.targetTouches[0].pageY - data.startY)) + 'px' : Math.floor(data.x + (e.clientX - data.startX)) + 'px,' + Math.floor(data.y + (e.clientY - data.startY)) + 'px';
-            node.style[transformStr] = transformStyle.replace('{$position}', type == 'touchmove' ? Math.floor(data.x + (e.targetTouches[0].pageX - data.startX)) + 'px,' + Math.floor(data.y + (e.targetTouches[0].pageY - data.startY)) + 'px' : Math.floor(data.x + (e.clientX - data.startX)) + 'px,' + Math.floor(data.y + (e.clientY - data.startY)) + 'px');
+            box.node.style[transformStr] = transformStyle.replace('{$position}', type === 'touchmove' ? Math.floor(data.x + (e.targetTouches[0].pageX - data.startX)) + 'px,' + Math.floor(data.y + (e.targetTouches[0].pageY - data.startY)) + 'px' : Math.floor(data.x + (e.clientX - data.startX)) + 'px,' + Math.floor(data.y + (e.clientY - data.startY)) + 'px');
           }
         }
         break;
@@ -135,8 +132,8 @@ function dragEvent (box) {
       case 'touchstart':
       case 'mousedown': {
         if (dragMode && box.movesNode.contains(e.target)) {
-          if (type == 'touchstart') {
-            if (e.targetTouches.length == 1) {
+          if (type === 'touchstart') {
+            if (e.targetTouches.length === 1) {
               data.startX = e.targetTouches[0].pageX;
               data.startY = e.targetTouches[0].pageY;
             } else
@@ -146,16 +143,16 @@ function dragEvent (box) {
             data.startY = e.clientY;
           }
 
-          moveEnable = true;
-          var position = /\(\s*([+-]?\d+(?:\.\d+)?)px\s*,\s*([+-]?\d+(?:\.\d+)?)/i.exec(node.style[transformStr]);
+          enable = true;
+          let position = /\(\s*([+-]?\d+(?:\.\d+)?)px\s*,\s*([+-]?\d+(?:\.\d+)?)/i.exec(box.node.style[transformStr]);
 
           data.x = +position[1];
           data.y = +position[2];
         } else
-          moveEnable = false;
+          enable = false;
 
         //异步执行
-        currentBox !== box && window.setTimeout(function () {
+        currentBox !== box && window.setTimeout(() => {
           box.activate();
         }, 0);
       }
@@ -163,13 +160,13 @@ function dragEvent (box) {
       //移动结束
       case 'touchend':
       case 'mouseup': {
-        moveEnable = false;
+        enable = false;
       }
       break;
       //移出弹窗
       case 'mouseout': {
-        if (moveEnable && !node.contains(e.relatedTarget)) {
-          moveEnable = false;
+        if (enable && !box.node.contains(e.relatedTarget)) {
+          enable = false;
         }
       }
       break;
@@ -180,20 +177,20 @@ function dragEvent (box) {
   } else {
     //top/left模式
     return function (e) {
-      var type = e.type;
+      const type = e.type;
       switch (type) {
         //移动中
         case 'touchmove':
         case 'mousemove': {
-          if (moveEnable) {
+          if (enable) {
             e.preventDefault();
             e.stopPropagation();
-            if (type == 'touchmove') {
-              node.style.left = data.x + (e.targetTouches[0].pageX - data.startX) + 'px';
-              node.style.top = data.y + (e.targetTouches[0].pageY - data.startY) + 'px';
+            if (type === 'touchmove') {
+              box.node.style.left = data.x + (e.targetTouches[0].pageX - data.startX) + 'px';
+              box.node.style.top = data.y + (e.targetTouches[0].pageY - data.startY) + 'px';
             } else {
-              node.style.left = data.x + (e.clientX - data.startX) + 'px';
-              node.style.top = data.y + (e.clientY - data.startY) + 'px';
+              box.node.style.left = data.x + (e.clientX - data.startX) + 'px';
+              box.node.style.top = data.y + (e.clientY - data.startY) + 'px';
             }
           }
         }
@@ -202,8 +199,8 @@ function dragEvent (box) {
       case 'touchstart':
       case 'mousedown': {
         if (dragMode && box.movesNode.contains(e.target)) {
-          if (type == 'touchstart') {
-            if (e.targetTouches.length == 1) {
+          if (type === 'touchstart') {
+            if (e.targetTouches.length === 1) {
               data.startX = e.targetTouches[0].pageX;
               data.startY = e.targetTouches[0].pageY;
             } else
@@ -213,14 +210,14 @@ function dragEvent (box) {
             data.startY = e.clientY;
           }
 
-          moveEnable = true;
-          data.x = node.offsetLeft;
-          data.y = node.offsetTop;
+          enable = true;
+          data.x = box.node.offsetLeft;
+          data.y = box.node.offsetTop;
         } else
-          moveEnable = false;
+          enable = false;
 
         //异步执行
-        currentBox !== box && window.setTimeout(function () {
+        currentBox !== box && window.setTimeout(() => {
           box.activate();
         }, 0);
       }
@@ -228,13 +225,13 @@ function dragEvent (box) {
       //移动结束
       case 'touchend':
       case 'mouseup': {
-        moveEnable = false;
+        enable = false;
       }
       break;
       //移出弹窗
       case 'mouseout': {
-        if (moveEnable && !node.contains(e.relatedTarget)) {
-          moveEnable = false;
+        if (enable && !box.node.contains(e.relatedTarget)) {
+          enable = false;
         }
       }
       break;
@@ -245,29 +242,28 @@ function dragEvent (box) {
   }
 }
 /*
- * 初始化数据和append node
+ * 初始化数据和create node
  *
  * @param string msg
  */
-function append (self) {
-  //清零option参数
-  self.nextBox = self.prevBox = null;
-  //append node
+function create (self) {
+  //create node
   const node = self.node = document.createElement('div');
   node.id = `${self.type}_box_${self.id}`;
   node.className = `${className} ${self.type}-box ${activeClassName} ${message.option.activeClassName} ${self.option.className}`;
   node.innerHTML = `<div class="${className}__head">${self.option.title}<span class="${className}__close" title="close/关闭">×</span></div><div class="${className}__body">${self.option.text}</div><div class="${className}__foot"></div></div>`;
+  rootNode.appendChild(node);
   //绑定关闭事件
-  let cssText = `position:fixed;z-index:${(message.option.zIndex > 0 ? message.option.zIndex : 10000) + counter};`,
+  let cssText = `position:fixed;z-index:${(typeof message.option.zIndex === 'number' && message.option.zIndex > 0 ? message.option.zIndex : 10000) + counter};`,
     i = 0,
     length = self.option.buttons.length,
     eventsName,
     event = dragEvent(self);
 
-  //append node
-  rootNode.appendChild(node);
-  if (self.option.transform && transform) {
-    cssText += 'left:50%;top:50%;' + (hasTransformPrefix ? `-${cssPrefix}-transform:translate(-${Math.floor(node.offsetWidth / 2)}px,-${Math.floor(node.offsetHeight / 2)}px);` : `transform:translate(-${Math.floor(node.offsetWidth / 2)}px,-${Math.floor(node.offsetHeight / 2)}px);`);
+  //create node
+  if (message.option.transform && transform) {
+    // cssText += 'left:50%;top:50%;' + (hasTransformPrefix ? `-${cssPrefix}-transform:translate(-${Math.floor(node.offsetWidth / 2)}px,-${Math.floor(node.offsetHeight / 2)}px);` : `transform:translate(-${Math.floor(node.offsetWidth / 2)}px,-${Math.floor(node.offsetHeight / 2)}px);`);
+    cssText += `left:50%;top:50%;${hasTransformPrefix ? `-${cssPrefix}-` : ''}transform:${transformStyle}`.replace('{$position}', `-${Math.floor(node.offsetWidth / 2)}px,-${Math.floor(node.offsetHeight / 2)}px`);
   } else {
     cssText += `left:${(document.documentElement.clientWidth - node.offsetWidth) / 2}px;top:${(document.documentElement.clientHeight - node.offsetHeight) / 2}px`;
   }
@@ -291,7 +287,7 @@ function append (self) {
     //允许移动
     eventsName = ['touchstart', 'touchmove', 'touchend', 'mousedown', 'mousemove', 'mouseup', 'mouseout'];
     //能拖动的node
-    self.movesNode = self.option.dragMode == 1 ? node : node.firstChild;
+    self.movesNode = self.option.dragMode === 1 ? node : node.firstChild;
     self.movesNode.classList.add('drag-enable');
   } else {
     eventsName = ['touchend', 'mouseup'];
@@ -304,56 +300,51 @@ function append (self) {
   if (currentBox) {
     try {
       self.prevBox = currentBox;
-      self.prevBox.nextBox = self;
-      self.prevBox.node.classList.remove(activeClassName, message.option.activeClassName);
+      currentBox.nextBox = self;
+      currentBox.node.classList.remove(activeClassName, message.option.activeClassName);
     } catch (e) {
-      console.log('append error:', e);
+      console.log('create error:', e);
     }
   }
-  self.promise = new Promise((resolve) => {
-    self.resolve = resolve;
-  });
   //currentBox指向当前对象
   currentBox = self;
   //打开蒙板
   self.option.showMask && mask.show();
+  self.promise = new Promise((resolve) => {
+    self.resolve = resolve;
+  });
   //message.length = counter;
   if (self.option.timeout) {
-    window.setTimeout(function () {
+    window.setTimeout(() => {
       try {
         self.remove(-3);
-      } catch (e) {}
+      } catch (e) {console.log(`remove box error: ${self.id}`)}
     }, self.option.timeout * 1000);
   }
 }
 
-/*
- * 退出
- */
-function exit(index = -1) {
-  counter && currentBox.remove(index);
-  !counter && mask.hide();
-}
-
 class Box {
-  constructor (option, events) {
+  /*
+   * @param object events {close, active}
+   */
+  constructor (text, events) {
     this.id = counter++;
     this.option = Object.assign(
       {},
       defaultOption,
-      typeof option === 'string' ? { option } : option);
+      typeof text === 'string' ? { text } : text);
     this.events = events;
+    this.nextBox = this.prevBox = this.node = null;
   }
   /*
    * 激活当前弹窗到顶层
    */
   activate () {
+    const node = this.node;
     let box,
-      node = this.node,
       zIndex = node.style.zIndex;
-    //必要时，判断currentBox == this
     //断开当前box对象
-    this.option.buttons.length && this.node.querySelector(`.${className}__foot>button:first-child`).focus();
+    this.option.buttons.length && node.querySelector(`.${className}__foot>button:first-child`).focus();
 
     //把当前box对象添加到链到末端
     if (currentBox !== this) {
@@ -382,85 +373,75 @@ class Box {
     }
     node.classList.add(activeClassName, message.option.activeClassName);
 
-    this.events && typeof this.events.active == 'function' && this.events.active(this);
+    this.events && typeof this.events.active === 'function' && this.events.active(this);
 
     return this;
   }
   /*
    * 销毁对象
    */
-  remove (index = -1) {
-      //console.log(isClean)
-      let node = this.node;
-
-      //移除node
-      node.parentNode.removeChild(node);
-      node = this.node = null;
-      //清理message列表
-      if (--counter) {
-        //当前活动弹窗（currentBox）
-        try {
-          if (this.prevBox)
-            this.prevBox.nextBox = this.nextBox;
-          if (this.nextBox) {
-            this.nextBox.prevBox = this.prevBox;
-          }
-          if (currentBox === this) {
-            currentBox = this.prevBox;
-            currentBox.activate();
-          }
-        } catch (e) {
-          console.error('remove error', e)
+  remove(index = -1) {
+    //移除node
+    this.node.parentNode.removeChild(this.node);
+    this.node = null;
+    this.resolve({
+      index
+    });
+    this.promise.catch(() => {});
+    this.events && typeof this.events.close === 'function' && this.events.close({
+      index
+    });
+    //清理message列表
+    if (--counter) {
+      //当前活动弹窗（currentBox）
+      try {
+        if (this.prevBox)
+          this.prevBox.nextBox = this.nextBox;
+        if (this.nextBox) {
+          this.nextBox.prevBox = this.prevBox;
         }
-      } else {
-        currentBox = null;
-        //关闭蒙版
-        !counter && mask.hide();
+        if (currentBox === this) {
+          currentBox = this.prevBox;
+          currentBox.activate();
+        }
+      } catch (e) {
+        console.error('remove error', e)
       }
-
-      this.resolve({
-        index
-      });
-      this.promise.catch(() => {});
-      if (this.events && typeof this.events.close == 'function') {
-        this.events.close({
-          index
-        });
-        this.callback = null;
-      }
-      this.events = this.prevBox = this.nextBox = null;
-
-      return this;
+    } else {
+      currentBox = null;
+      //关闭蒙版
+      !counter && mask.hide();
     }
-    /*
-     *  关闭事件
-     *  -3 timeout -2 键盘esc -1 点击遮罩层 0 关闭按钮 1~ 底部按钮
-     */
-    close (cb) {
-      this.promise = this.promise.then(cb);
 
-      return this;
-    }
+    this.events = this.prevBox = this.nextBox = null;
+  }
+  /*
+   *  关闭事件
+   *  -3 timeout -2 键盘esc -1 点击遮罩层 0 关闭按钮 1~ 底部按钮
+   */
+  close(cb) {
+    this.promise = this.promise.then(cb);
+  }
 }
 class AlertBox extends Box {
   constructor (text, events) {
     super({ text }, events);
     this.type = 'alert';
-    append(this);
+    create(this);
   }
 }
 class ConfirmBox extends Box {
-  constructor(text, events) {
+  constructor (text, events) {
     super({
       text,
       buttons: ['确认', '取消']
     }, events);
     this.type = 'confirm';
-    append(this);
+    create(this);
   }
 }
 class ToastBox extends Box {
-  constructor(text, timeout = 3) {
+  constructor (text, timeout = 3) {
     super({
       text,
       timeout,
@@ -469,14 +450,14 @@ class ToastBox extends Box {
       buttons: []
     });
     this.type = 'toast';
-    append(this);
+    create(this);
   }
 }
 class PopBox extends Box {
-  constructor(option, events) {
-    super(option, events);
+  constructor (text, events) {
+    super(text, events);
     this.type = 'pop';
-    append(this);
+    create(this);
   }
 }
 export const alert = (text, events) => {
